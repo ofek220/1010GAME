3707ef8d9ad8716a6deccffbdb2c572182ef44b7
ofek <ofekoz004@gmail.com>
2025-09-19 00:16:26 +0300

    Update index.html and vite.config.js for Vite + React

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "/1010GAME/",
   plugins: [react()],
   build: {
     outDir: "dist",
     assetsDir: "assets",
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+      output: {
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split(".");
+          const ext = info[info.length - 1];
+          if (/\.(css|scss)$/.test(assetInfo.name)) {
+            return `assets/styles.[ext]`;
+          }
+          return `assets/[name].[ext]`;
+        },
+      },
+    },
   },
 });
